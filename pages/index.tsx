b2c1af07d47e43cbe862b24a65f0c1e6e5d46d5b
Joncin Picol <jctrilamanila@gmail.com>
2022-02-19 03:51:22 -0500

    created hero

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import Head from 'next/head'
 import Header from '../components/Header'
+import Hero from '../components/Hero'
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Header />
-    </div>
+      <Hero />
+    </>
   )
 }
