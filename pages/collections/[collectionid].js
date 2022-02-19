dc5c55e6038f67c5a688673c0cafea335852d4bb
Joncin Picol <jctrilamanila@gmail.com>
2022-02-19 03:18:26 -0500

    created navbar

---
+++ pages/collections/[collectionid].js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+
+const Collection = () => {
+  const router = useRouter()
+  return <div></div>
+}
+
+export default Collection
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,10 @@
 import Head from 'next/head'
+import Header from '../components/Header'
 
 export default function Home() {
-  return <h2>Yo</h2>
+  return (
+    <div>
+      <Header />
+    </div>
+  )
 }
